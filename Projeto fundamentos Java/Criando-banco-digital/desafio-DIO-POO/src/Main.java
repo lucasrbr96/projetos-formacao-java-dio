b07cdd15cf49697171f6dbf692da5e14a4f96ba6
Lucas <lucasramosgab90@gmail.com>
2023-06-22 12:26:46 -0300

    Abstraindo Bootcamp DIO - Desafio POO

---
+++ Projeto fundamentos Java/Criando-banco-digital/desafio-DIO-POO/src/Main.java
@@ -0,0 +1,45 @@
+import br.com.dio.desafio.dominio.*;
+
+import java.time.LocalDate;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso java");
+        curso1.setDescricao("Descrição curso java");
+        curso1.setCargaHoraria(10);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso javascript");
+        curso2.setDescricao("Descrição curso javascript");
+        curso2.setCargaHoraria(10);
+
+
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição mentoria Java");
+        mentoria.setData(LocalDate.now());
+        System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("bootcamp Java developer");
+        bootcamp.setDescricao("Descricao Bootcamp");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devLucas = new Dev();
+        devLucas.setNome("Lucas");
+        devLucas.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscrito "+ devLucas.getConteudosInscrito());
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("Joao");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscrito "+ devJoao.getConteudosInscrito());
+
+    }
+}
